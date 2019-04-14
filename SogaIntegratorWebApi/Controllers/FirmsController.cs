864e253247720b9921af49f24fc1133ecae8d5e5
Jarosław Kłębucki <j.klebucki@ajksoftware.pl>
2019-04-15 00:22:52 +0200

    Added DocumentPositionsController, GoodsController

--- SogaIntegratorWebApi/Controllers/FirmsController.cs
+++ SogaIntegratorWebApi/Controllers/FirmsController.cs
@@ -46,7 +46,7 @@ namespace SogaIntegratorWebApi.Controllers
                 "NIP_FI, TEL_FI, FAX_FI, UWAGI_FI, KONTO, BANK, MAIL_FI, WWW, RR, NR_DOW, WYDAWCA_DOW, ANALITYKA, " +
                 "ID_KR, DEL, DAKT, DATA_INS, ID_UZ_INS, ID_ZR " +
                 "from firmy where " +
-                "ID_FI=" + id;
+                "ID_FI IN (" + id + ")";
                 fbCommand.Connection = fbConnection;
                 var reader = fbCommand.ExecuteReader();
                 while (reader.Read())
@@ -99,80 +99,5 @@ namespace SogaIntegratorWebApi.Controllers
             return new JsonResult(list[0], responseJson);
         }
 
-        // [HttpGet]
-        // [HttpGet("{id0}/{id1}")]
-        // public IActionResult Get(string id0, string id1)
-        // {
-        //     string connectionString = firebaseConfig.Value.ConnectionString();
-
-        //     FbConnection fbConnection = new FbConnection(connectionString);
-        //     List<Firma> list = new List<Firma>();
-        //     if (string.IsNullOrEmpty(id0))
-        //     {
-        //         return BadRequest();
-        //     }
-        //     try
-        //     {
-        //         fbConnection.Open();
-
-        //         FbCommand fbCommand = new FbCommand();
-
-        //         fbCommand.CommandText =
-        //         "select  ID_FI, KOD_FI, NAZWA_FI_1, NAZWA_FI_2, K_P_FI, MIASTO_FI, ULICA_FI, NR_FI, NR_L_FI, " +
-        //         "NIP_FI, TEL_FI, FAX_FI, UWAGI_FI, KONTO, BANK, MAIL_FI, WWW, RR, NR_DOW, WYDAWCA_DOW, ANALITYKA, " +
-        //         "ID_KR, DEL, DAKT, DATA_INS, ID_UZ_INS, ID_ZR " +
-        //         "from firmy where " +
-        //         "ID_FI in " + id0;
-        //         fbCommand.Connection = fbConnection;
-        //         var reader = fbCommand.ExecuteReader();
-        //         while (reader.Read())
-        //         {
-        //             list.Add(new Firma
-        //             {
-        //                 ID_FI = reader.GetInt32(0), //ID_FI
-        //                 KOD_FI = reader.GetString(1), //KOD_FI
-        //                 NAZWA_FI_1 = reader.GetString(2), //NAZWA_FI_1
-        //                 NAZWA_FI_2 = reader.GetString(3), //NAZWA_FI_2
-        //                 K_P_FI = reader.GetString(4), //K_P_FI
-        //                 MIASTO_FI = reader.GetString(5), //MIASTO_FI
-        //                 ULICA_FI = reader.GetString(6), //ULICA_FI
-        //                 NR_FI = reader.GetString(7), //NR_FI
-        //                 NR_L_FI = reader.GetString(8), //NR_L_FI
-        //                 NIP_FI = reader.GetString(9), //NIP_FI
-        //                 TEL_FI = reader.GetString(10), //TEL_FI
-        //                 FAX_FI = reader.GetString(11), //FAX_FI
-        //                 UWAGI_FI = reader.GetString(12), //UWAGI_FI
-        //                 KONTO = reader.GetString(13), //KONTO
-        //                 BANK = reader.GetString(14), //BANK
-        //                 MAIL_FI = reader.GetString(15), //MAIL_FI
-        //                 WWW = reader.GetString(16), //WWW
-        //                 RR = reader.GetInt32(17), //RR
-        //                 NR_DOW = reader.GetString(18), //NR_DOW
-        //                 WYDAWCA_DOW = reader.GetString(19), //WYDAWCA_DOW
-        //                 ANALITYKA = reader.GetString(20), //ANALITYKA
-        //                 ID_KR = reader.GetInt32(21), //ID_KR
-        //                 DEL = reader.GetInt32(22), //DEL
-        //                 DAKT = reader.GetDateTime(23), //DAKT
-        //                 DATA_INS = reader.GetDateTime(24), //DATA_INS
-        //                 ID_UZ_INS = reader.GetInt32(25), //ID_UZ_INS
-        //                 ID_ZR = reader.GetInt32(26), //ID_ZR
-        //             });
-        //         }
-
-        //         fbConnection.Close();
-
-        //     }
-        //     catch (Exception e)
-        //     {
-        //         Console.WriteLine(e.Message);
-        //     }
-
-        //     JsonSerializerSettings responseJson = new JsonSerializerSettings
-        //     {
-        //         Formatting = Formatting.Indented,
-        //         DateFormatString = "yyyy-MM-dd HH:mm:ss"
-        //     };
-        //     return new JsonResult(list, responseJson);
-        // }
     }
 }
--- SogaIntegratorWebApi/Controllers/GoodsController.cs
+++ SogaIntegratorWebApi/Controllers/GoodsController.cs
@@ -20,15 +20,28 @@ namespace SogaIntegratorWebApi.Controllers
         {
             this.firebaseConfig = firebaseConfig;
         }
-        // GET api/values/5
-        [HttpGet]
-        public IActionResult Get()
+
+        /// <summary>
+        /// Return goods by parametr name and value 
+        /// </summary>
+        /// <param name="parName"></param>
+        /// <param name="parValue"></param>
+        /// <returns></returns>
+        [HttpGet("{parName}/{parValue}")]
+        public IActionResult Get(string parName, string parValue)
         {
             string connectionString = firebaseConfig.Value.ConnectionString();
-
+            string whereCondition = "";
             FbConnection fbConnection = new FbConnection(connectionString);
             List<Towar> list = new List<Towar>();
-
+            switch (parName)
+            {
+                case "name":
+                    whereCondition = "where NAZWA_TW LIKE '%" + parValue + "%'";
+                    break;
+                default:
+                    break;
+            }
             try
             {
                 fbConnection.Open();
@@ -41,63 +54,12 @@ namespace SogaIntegratorWebApi.Controllers
                         "CENA_SP_N_4, CENA_SP_B_4, CENA_SP_N_5, CENA_SP_B_5, MAX_RABAT, ZN_WAGA, " +
                         "ZN_CENA, FISK, POCKET, WWW, ID_GRZA, DEL, CZAS_STAN, CZAS_OS, ILOSC_OS, KOLOR_TW, " +
                         "KALORIE, PKT, NR_DF, FA_USLUGA, DAKT, ID_DR_2, WAGA_AUTO " +
-                        "from towary";
+                        "from towary " +
+                        whereCondition;
 
                 fbCommand.Connection = fbConnection;
                 var reader = fbCommand.ExecuteReader();
-                while (reader.Read())
-                {
-                    list.Add(new Towar
-                    {
-                        ID_TW = reader.GetInt32(0), //ID_TW
-                        TYP_TW = reader.GetString(1), //TYP_TW
-                        KOD_TW = reader.GetString(2), //KOD_TW
-                        NAZWA_TW = reader.GetString(3), //NAZWA_TW
-                        SWW_TW = reader.GetString(4), //SWW_TW
-                        JED_TW = reader.GetString(5), //JED_TW
-                        CENA_ZAK_N = reader.GetDecimal(6), //CENA_ZAK_N
-                        CENA_ZAK_B = reader.GetDecimal(7), //CENA_ZAK_B
-                        ID_VAT = reader.GetInt32(8), //ID_VAT
-                        VAT_P = reader.GetDecimal(9), //VAT_P
-                        STAN_TW = reader.GetDecimal(10), //STAN_TW
-                        ID_GR = reader.GetInt32(11), //ID_GR
-                        UWAGI = reader?.GetString(12), //UWAGI
-                        PRIORYTET = reader.GetInt32(13), //PRIORYTET
-                        PLU = !reader.IsDBNull(14) ? reader.GetInt32(14) : 0, //PLU
-                        ID_DR = reader.GetInt32(15), //ID_DR
-                        KOD_KR = reader?.GetString(16), //KOD_KR
-                        CENA_SP_N_1 = reader.GetDecimal(17), //CENA_SP_N_1
-                        CENA_SP_B_1 = reader.GetDecimal(18), //CENA_SP_B_1
-                        CENA_SP_N_2 = reader.GetDecimal(19), //CENA_SP_N_2
-                        CENA_SP_B_2 = reader.GetDecimal(20), //CENA_SP_B_2
-                        CENA_SP_N_3 = reader.GetDecimal(21), //CENA_SP_N_3
-                        CENA_SP_B_3 = reader.GetDecimal(22), //CENA_SP_B_3
-                        CENA_SP_N_4 = reader.GetDecimal(23), //CENA_SP_N_4
-                        CENA_SP_B_4 = reader.GetDecimal(24), //CENA_SP_B_4
-                        CENA_SP_N_5 = reader.GetDecimal(25), //CENA_SP_N_5
-                        CENA_SP_B_5 = reader.GetDecimal(26), //CENA_SP_B_5
-                        MAX_RABAT = reader.GetInt32(27), //MAX_RABAT
-                        ZN_WAGA = reader.GetInt32(28), //ZN_WAGA
-                        ZN_CENA = reader.GetInt32(29), //ZN_CENA
-                        FISK = reader.GetInt32(30), //FISK
-                        POCKET = reader.GetInt32(31), //POCKET
-                        WWW = reader.GetInt32(32), //WWW
-                        ID_GRZA = reader.GetInt32(33), //ID_GRZA
-                        DEL = reader.GetInt32(34), //DEL
-                        CZAS_STAN = reader.GetInt32(35), //CZAS_STAN
-                        CZAS_OS = reader.GetInt32(36), //CZAS_OS
-                        ILOSC_OS = reader.GetInt32(37), //ILOSC_OS
-                        KOLOR_TW = reader.GetInt32(38), //KOLOR_TW
-                        KALORIE = reader.GetDecimal(39), //KALORIE
-                        PKT = reader.GetInt32(40), //PKT
-                        NR_DF = reader.GetInt32(41), //NR_DF
-                        FA_USLUGA = reader.GetInt32(42), //FA_USLUGA
-                        DAKT = reader.GetDateTime(43), //DAKT
-                        ID_DR_2 = reader.GetInt32(44), //ID_DR_2
-                        WAGA_AUTO = reader.GetInt32(45), //WAGA_AUTO
-                    });
-                }
-
+                list = GenerateGoodsList(reader);
                 fbConnection.Close();
 
             }
@@ -113,9 +75,14 @@ namespace SogaIntegratorWebApi.Controllers
             };
             return new JsonResult(list, responseJson);
         }
-        // GET api/values/5
-        [HttpGet("{ids}")]
-        public IActionResult Get(string ids)
+
+        /// <summary>
+        /// Return goods by id elements list separated by comma ","
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpGet("{id}")]
+        public IActionResult Get(string id)
         {
             string connectionString = firebaseConfig.Value.ConnectionString();
 
@@ -134,63 +101,11 @@ namespace SogaIntegratorWebApi.Controllers
                         "CENA_SP_N_4, CENA_SP_B_4, CENA_SP_N_5, CENA_SP_B_5, MAX_RABAT, ZN_WAGA, " +
                         "ZN_CENA, FISK, POCKET, WWW, ID_GRZA, DEL, CZAS_STAN, CZAS_OS, ILOSC_OS, KOLOR_TW, " +
                         "KALORIE, PKT, NR_DF, FA_USLUGA, DAKT, ID_DR_2, WAGA_AUTO " +
-                        "from towary where ID_TW in (" + ids + ")";
+                        "from towary where ID_TW in (" + id + ")";
 
                 fbCommand.Connection = fbConnection;
                 var reader = fbCommand.ExecuteReader();
-                while (reader.Read())
-                {
-                    list.Add(new Towar
-                    {
-                        ID_TW = reader.GetInt32(0), //ID_TW
-                        TYP_TW = reader.GetString(1), //TYP_TW
-                        KOD_TW = reader.GetString(2), //KOD_TW
-                        NAZWA_TW = reader.GetString(3), //NAZWA_TW
-                        SWW_TW = reader.GetString(4), //SWW_TW
-                        JED_TW = reader.GetString(5), //JED_TW
-                        CENA_ZAK_N = reader.GetDecimal(6), //CENA_ZAK_N
-                        CENA_ZAK_B = reader.GetDecimal(7), //CENA_ZAK_B
-                        ID_VAT = reader.GetInt32(8), //ID_VAT
-                        VAT_P = reader.GetDecimal(9), //VAT_P
-                        STAN_TW = reader.GetDecimal(10), //STAN_TW
-                        ID_GR = reader.GetInt32(11), //ID_GR
-                        UWAGI = reader?.GetString(12), //UWAGI
-                        PRIORYTET = reader.GetInt32(13), //PRIORYTET
-                        PLU = !reader.IsDBNull(14) ? reader.GetInt32(14) : 0, //PLU
-                        ID_DR = reader.GetInt32(15), //ID_DR
-                        KOD_KR = reader?.GetString(16), //KOD_KR
-                        CENA_SP_N_1 = reader.GetDecimal(17), //CENA_SP_N_1
-                        CENA_SP_B_1 = reader.GetDecimal(18), //CENA_SP_B_1
-                        CENA_SP_N_2 = reader.GetDecimal(19), //CENA_SP_N_2
-                        CENA_SP_B_2 = reader.GetDecimal(20), //CENA_SP_B_2
-                        CENA_SP_N_3 = reader.GetDecimal(21), //CENA_SP_N_3
-                        CENA_SP_B_3 = reader.GetDecimal(22), //CENA_SP_B_3
-                        CENA_SP_N_4 = reader.GetDecimal(23), //CENA_SP_N_4
-                        CENA_SP_B_4 = reader.GetDecimal(24), //CENA_SP_B_4
-                        CENA_SP_N_5 = reader.GetDecimal(25), //CENA_SP_N_5
-                        CENA_SP_B_5 = reader.GetDecimal(26), //CENA_SP_B_5
-                        MAX_RABAT = reader.GetInt32(27), //MAX_RABAT
-                        ZN_WAGA = reader.GetInt32(28), //ZN_WAGA
-                        ZN_CENA = reader.GetInt32(29), //ZN_CENA
-                        FISK = reader.GetInt32(30), //FISK
-                        POCKET = reader.GetInt32(31), //POCKET
-                        WWW = reader.GetInt32(32), //WWW
-                        ID_GRZA = reader.GetInt32(33), //ID_GRZA
-                        DEL = reader.GetInt32(34), //DEL
-                        CZAS_STAN = reader.GetInt32(35), //CZAS_STAN
-                        CZAS_OS = reader.GetInt32(36), //CZAS_OS
-                        ILOSC_OS = reader.GetInt32(37), //ILOSC_OS
-                        KOLOR_TW = reader.GetInt32(38), //KOLOR_TW
-                        KALORIE = reader.GetDecimal(39), //KALORIE
-                        PKT = reader.GetInt32(40), //PKT
-                        NR_DF = reader.GetInt32(41), //NR_DF
-                        FA_USLUGA = reader.GetInt32(42), //FA_USLUGA
-                        DAKT = reader.GetDateTime(43), //DAKT
-                        ID_DR_2 = reader.GetInt32(44), //ID_DR_2
-                        WAGA_AUTO = reader.GetInt32(45), //WAGA_AUTO
-                    });
-                }
-
+                list = GenerateGoodsList(reader);
                 fbConnection.Close();
 
             }
@@ -205,6 +120,69 @@ namespace SogaIntegratorWebApi.Controllers
                 DateFormatString = "yyyy-MM-dd HH:mm:ss"
             };
             return new JsonResult(list, responseJson);
+        }
+
+        /// <summary>
+        /// Return list of Towar reading FbDataReader 
+        /// </summary>
+        /// <param name="reader"></param>
+        /// <returns></returns>
+        private List<Towar> GenerateGoodsList(FbDataReader reader)
+        {
+            List<Towar> list = new List<Towar>();
+            while (reader.Read())
+            {
+                list.Add(new Towar
+                {
+                    ID_TW = reader.GetInt32(0), //ID_TW
+                    TYP_TW = reader.GetString(1), //TYP_TW
+                    KOD_TW = reader.GetString(2), //KOD_TW
+                    NAZWA_TW = reader.GetString(3), //NAZWA_TW
+                    SWW_TW = reader.GetString(4), //SWW_TW
+                    JED_TW = reader.GetString(5), //JED_TW
+                    CENA_ZAK_N = reader.GetDecimal(6), //CENA_ZAK_N
+                    CENA_ZAK_B = reader.GetDecimal(7), //CENA_ZAK_B
+                    ID_VAT = reader.GetInt32(8), //ID_VAT
+                    VAT_P = reader.GetDecimal(9), //VAT_P
+                    STAN_TW = reader.GetDecimal(10), //STAN_TW
+                    ID_GR = reader.GetInt32(11), //ID_GR
+                    UWAGI = reader?.GetString(12), //UWAGI
+                    PRIORYTET = reader.GetInt32(13), //PRIORYTET
+                    PLU = !reader.IsDBNull(14) ? reader.GetInt32(14) : 0, //PLU
+                    ID_DR = reader.GetInt32(15), //ID_DR
+                    KOD_KR = reader?.GetString(16), //KOD_KR
+                    CENA_SP_N_1 = reader.GetDecimal(17), //CENA_SP_N_1
+                    CENA_SP_B_1 = reader.GetDecimal(18), //CENA_SP_B_1
+                    CENA_SP_N_2 = reader.GetDecimal(19), //CENA_SP_N_2
+                    CENA_SP_B_2 = reader.GetDecimal(20), //CENA_SP_B_2
+                    CENA_SP_N_3 = reader.GetDecimal(21), //CENA_SP_N_3
+                    CENA_SP_B_3 = reader.GetDecimal(22), //CENA_SP_B_3
+                    CENA_SP_N_4 = reader.GetDecimal(23), //CENA_SP_N_4
+                    CENA_SP_B_4 = reader.GetDecimal(24), //CENA_SP_B_4
+                    CENA_SP_N_5 = reader.GetDecimal(25), //CENA_SP_N_5
+                    CENA_SP_B_5 = reader.GetDecimal(26), //CENA_SP_B_5
+                    MAX_RABAT = reader.GetInt32(27), //MAX_RABAT
+                    ZN_WAGA = reader.GetInt32(28), //ZN_WAGA
+                    ZN_CENA = reader.GetInt32(29), //ZN_CENA
+                    FISK = reader.GetInt32(30), //FISK
+                    POCKET = reader.GetInt32(31), //POCKET
+                    WWW = reader.GetInt32(32), //WWW
+                    ID_GRZA = reader.GetInt32(33), //ID_GRZA
+                    DEL = reader.GetInt32(34), //DEL
+                    CZAS_STAN = reader.GetInt32(35), //CZAS_STAN
+                    CZAS_OS = reader.GetInt32(36), //CZAS_OS
+                    ILOSC_OS = reader.GetInt32(37), //ILOSC_OS
+                    KOLOR_TW = reader.GetInt32(38), //KOLOR_TW
+                    KALORIE = reader.GetDecimal(39), //KALORIE
+                    PKT = reader.GetInt32(40), //PKT
+                    NR_DF = reader.GetInt32(41), //NR_DF
+                    FA_USLUGA = reader.GetInt32(42), //FA_USLUGA
+                    DAKT = reader.GetDateTime(43), //DAKT
+                    ID_DR_2 = reader.GetInt32(44), //ID_DR_2
+                    WAGA_AUTO = reader.GetInt32(45), //WAGA_AUTO
+                });
+            }
+            return list;
         }
     }
 }
